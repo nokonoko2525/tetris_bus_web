903ae613483846c87f7df666339d24d2b33075d4
nokonoko2525 <c1407448@st.kanazawa-it.ac.jp>
2024-12-18 17:09:33 +0900

    いらないファイルの削除

--- src/app/components/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-    reactStrictMode: true,
-    assetPrefix: process.env.GITHUB_REPOSITORY
-      ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
-      : '',
-  }
